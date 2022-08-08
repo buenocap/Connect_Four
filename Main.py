c4122e388543422143c37d5cf4f54dd76835b9a4
Pedro Bueno <pbueno159@gmail.com>
2022-08-08 12:54:49 -0600

    Revisiting place_chip functionality

--- Main.py
+++ Main.py
@@ -10,30 +10,49 @@ class Tic_Tac_Toe:
     def create_board():
         game_board = np.zeros((6,7))
         return game_board
+
     def place_chip(self,pos_y,player):
-        full_column = "Column is full please make another selection!\n"
-        for pos_x in reversed(range(0,6)):
-            if self.board[pos_x][pos_y] == 0:
-                self.board[pos_x][pos_y] = player
-                return pos_x,pos_y
+        full_msg = "Column is full plese make another selection!\n"
+        for x in reversed(range(0,len(self.board))):
+            if self.board[x][pos_y] == 0:
+                self.board[x][pos_y] = player
+                return x,pos_y,player
             else:
                 pass
-        return full_column
-    def checkboard(self,pos_x,pos_y):
+        print(full_msg)
+        return 0,0,0
+
+    def checkboard(self,pos_x,pos_y,player):
+        counter = 0
         # Check left
         # Check right
         # Check up
         # Check down
         # Check diagonal right
         # Check diagonal left
-        pass
+        return
 
     # Game Variables
     board = create_board()
 
 # Create Board
 game = Tic_Tac_Toe()
+
+# Create Player and Computer
+token = input("Would you like to go first or second?\n").lower()
+if token == 'first':
+    player = Player.Player(1)
+    computer = COM.Computer(2)
+else:
+    player = Player.Player(2)
+    computer = COM.Computer(1)
+
+#Start Game
 while game.game_status != True:
-    selection = int(input("Enter a row:\n"))
-    game.place_chip(selection,1)
+    try:
+        selection = int(input("Enter a row:\n"))
+    except ValueError:
+        print("invalid entry")
+
+    x,y,user_token = game.place_chip(selection,1)
     print(game.board)
